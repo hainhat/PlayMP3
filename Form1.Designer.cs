2be3256fb7ea1cfbf0606d9ed06db6080016ae02
hainhat <tohainhat17062003@gmail.com>
2024-04-23 01:25:14 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace PlayMp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listMusic = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btPlay = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.btStop = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listMusic
+            // 
+            this.listMusic.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listMusic.HideSelection = false;
+            this.listMusic.Location = new System.Drawing.Point(77, 17);
+            this.listMusic.Name = "listMusic";
+            this.listMusic.Size = new System.Drawing.Size(597, 243);
+            this.listMusic.TabIndex = 0;
+            this.listMusic.UseCompatibleStateImageBehavior = false;
+            this.listMusic.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "STT";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên";
+            this.columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Đường dẫn";
+            this.columnHeader3.Width = 240;
+            // 
+            // btPlay
+            // 
+            this.btPlay.Location = new System.Drawing.Point(334, 307);
+            this.btPlay.Name = "btPlay";
+            this.btPlay.Size = new System.Drawing.Size(75, 23);
+            this.btPlay.TabIndex = 1;
+            this.btPlay.Text = "Play";
+            this.btPlay.UseVisualStyleBackColor = true;
+            this.btPlay.Click += new System.EventHandler(this.btPlay_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(21, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(753, 378);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btBrowse);
+            this.tabPage1.Controls.Add(this.btAdd);
+            this.tabPage1.Controls.Add(this.txtPath);
+            this.tabPage1.Controls.Add(this.txtName);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(745, 349);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Thêm nhạc";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(423, 91);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btBrowse.TabIndex = 1;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(309, 172);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 1;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(295, 91);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(100, 22);
+            this.txtPath.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(295, 45);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.hScrollBar1);
+            this.tabPage2.Controls.Add(this.btStop);
+            this.tabPage2.Controls.Add(this.listMusic);
+            this.tabPage2.Controls.Add(this.btPlay);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(745, 349);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Danh sách nhạc";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(221, 267);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(286, 21);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(458, 307);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(75, 23);
+            this.btStop.TabIndex = 2;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "PlayMP3";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listMusic;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btPlay;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace PlayMp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "File MP3|*.mp3";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string path = txtPath.Text;
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(path))
+            {
+                ListViewItem item = new ListViewItem(listMusic.Items.Count.ToString());
+                item.SubItems.Add(name);
+                item.SubItems.Add(path);
+                listMusic.Items.Add(item);
+
+                // Xóa dữ liệu đã nhập sau khi thêm vào ListView
+                txtName.Clear();
+                txtPath.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập tên và đường dẫn của bài nhạc!");
+            }
+        }
+
+        private void btPlay_Click(object sender, EventArgs e)
+        {
+            if (listMusic.SelectedItems.Count > 0)
+            {
+                string selectedPath = listMusic.SelectedItems[0].SubItems[2].Text; // Lấy đường dẫn từ dòng được chọn
+
+                if (outputDevice != null)
+                {
+                    outputDevice.Stop(); // Dừng nếu đang phát nhạc khác
+                }
+
+                outputDevice = new WaveOutEvent();
+                audioFile = new AudioFileReader(selectedPath);
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một bài hát để phát.");
+            }
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            if (outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                outputDevice.Stop();
+            }
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+                // Di chuyển đến vị trí trong nhạc tương ứng với giá trị của scrollbar
+                float percentage = (float)hScrollBar1.Value / hScrollBar1.Maximum;
+                audioFile.CurrentTime = TimeSpan.FromSeconds(percentage * audioFile.TotalTime.TotalSeconds);
+
+        }
+    }
+}
